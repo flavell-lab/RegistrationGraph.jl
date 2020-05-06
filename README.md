532f51b8220399e6d043ebb93ca720939d29b038
goldenratio1618 <adamatanas@gmail.com>
2020-05-06 05:11:35 -0400

    Initial commit. Added graph initialization functionality.

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # RegistrationGraph.jl
-A collection of tools for running elastix registration.
+A collection of tools for running elastix registration. Suppose there is a set of $N$ frames to be registered, but the frames are too dissimilar to be able to ensure a quality registration for each pair of frames. In this case, it is often helpful to generate a heuristic to evaluate how similar two frames are to each other (and hence, how likely elastix will be to succeed). Once such a heuristic can be determined, registration problems can be selected to minimize difficulty and maximum path length, as a graph optimization problem. This package provides several heuristics for various registration problems, graph theory solutions for constructing the registration problem graph, and automated syncing of scripts and data to the OpenMind server.
+
+
+## Frame Similarity Heuristics
+
+## Data Filters and Background Removal
+
+
+## Generating a Registration Problem Graph
+As a prerequisite for this step, it is assumed that you have a heuristic for elastix registration difficulty between frames, which has been used to generate an elastix difficutly file of the appropriate format. If you're using one of the heuristics above, the formatting will already have been done; otherwise, the correct format is a comma-separated list of frames for which you have the difficulty, followed by a space/newline-separated matrix whose $[i,j]$th entry is the heuristic dissimilarity of frame $i$ to frame $j$. Both the list of frames and the matrix are allowed to use the `[` and `]` characters, which will be ignored in the parsing. Furthermore, the matrix is allowed to be triangular, and not all frames need be included in the heuristic calculation, as long as the indexing of the images is consistent with the indexing of the matrix. The frame numbers must be integers. Example for a 7-frame video of which 3 frames were discarded, and the difficulties were $d_{13} = 5$, $d_{15} = 7$, $d_{17} = 8$, $d_{35} = 1$, $d_{37} = 6$, $d_{57} = 10$:
+
+```
+[1,3,5,7]
+[0.0 5.0 7.0 8.0
+ 0.0 0.0 1.0 6.0
+ 0.0 0.0 0.0 10.0
+ 0.0 0.0 0.0 0.0]
+```
+
+Suppose that data was saved in a file `elastix_difficulty.txt`. Then you can perform the following actions to generate a registration graph from it.
+
+```
+# loads the file into a graph
+# you may want to modify the difficulty_importance parameter
+# see documentation for load_graph
+graph = load_graph("/path/to/elastix_difficulty.txt")
+
+# after generating the difficulty file, you decided that frame 3 has bad data
+# so you want to prevent it from being part of the registration problems
+remove_frame!(graph, 3)
+
+# generates set of registration problems from the graph
+min_ind, subgraph, maximum_problem_chain = optimize_subgraph(graph)
+
+# plot the subgraph to visualize it with the GraphPlot package
+# isolated nodes are frames that will not be registered
+gplot(subgraph, layout=spring_layout, arrowlengthfrac=0.03)
+
+# saves subgraph to a text file containing a list of edges
+output_graph(subgraph, "/path/to/output_file.txt")
+```
+
+## Running Elastix on Openmind
+### `rsync`
+A prerequisite of this code is that the `rsync` program installed on the local computer. On Mac and Linux, this should be installed by default, or be installable via the default package manager. On Windows, it is recommended to install Ubuntu (Windows Subsystem for Linux) through the Windows Store and run the code from the Ubuntu terminal.
+
+### ssh keys
+Another prerequisite of this code is that you have set up ssh keys to OpenMind. 
+
+
+## Checking Elastix Quality and Regenerating the Graph
+After running elastix on a set of registration problems, it is likely that most, but not all, of them will succeed. 
---
+++ src/make_registration_graph.jl
@@ -0,0 +1,159 @@
+""" 
+Computes the minimum path between a node and all other nodes using Dijkstra's algorithm.
+# Arguments
+- `graph::SimpleWeightedGraph`: a graph represented as a weighted adjacency matrix
+- `node::Integer`: a node in the graph, represented as an index into `adj`
+# Returns:
+- `dist::Dict`: a dictionary of distances from each vertex to the `node` vertex
+- `paths::Dict`: a dictionary of shortest paths from each vertex to the `node` vertex
+"""
+function shortest_paths(graph::SimpleWeightedGraph, node::Integer)
+    dist = Dict()
+    paths = Dict()
+    unvisited = collect(1:size(adj)[1])
+    # initialize distances
+    for i=1:size(adj)[1]
+        dist[i] = Inf
+        paths[i] = []
+    end
+    dist[node] = 0
+    paths[node] = [node]
+    while length(unvisited) > 0
+        min_d, idx = findmin([dist[i] for i in unvisited])
+        if min_d == Inf
+            break
+        end
+        current = unvisited[idx]
+        for i in unvisited
+            new_dist = dist[current] + adj[current, i]
+            if new_dist < dist[i]
+                dist[i] = new_dist
+                paths[i] = [paths[current]; i]
+            end
+        end
+        unvisited = filter(x->(x!=current), unvisited)
+    end
+    return (dist, paths)
+end
+
+"""
+Converts a `graph::SimpleWeightedGraph` into an adjacency dictionary
+node => Dict({neighbor1 => weight1, neighbor2=> weight2, ...})
+"""
+function to_dict(graph::SimpleWeightedGraph)
+    dict = Dict()
+    for edge in edges(graph)
+        if !(src(edge) in keys(dict))
+            dict[src(edge)] = Dict()
+        end
+        if !(dst(edge) in keys(dict))
+            dict[dst(edge)] = Dict()
+        end
+        dict[src(edge)][dst(edge)] = weight(edge)
+        dict[dst(edge)][src(edge)] = weight(edge)
+    end
+    return dict
+end
+
+
+"""
+Finds the minimum node of a graph, which has the smallest average shortest path length
+to each other node in that graph. Unconnected nodes are counted as having a path length equal to the
+highest edge weight.
+# Parameters
+- `graph::SimpleWeightedGraph`: a graph
+# Returns:
+- `min_node::Integer`: the minimum node
+- `subgraph::SimpleWeightedDiGraph`: an unweighted graph whose edges consist of shortest paths from the minimum node of the original graph to other nodes.
+- `maximum_problem_chain::Integer`: the number of vertices in the longest chain of registration problems in the subgraph.
+This graph is directed, and only has edges going away from the minimum node.
+"""
+function optimize_subgraph(graph::SimpleWeightedGraph)
+    dist = []
+    paths = []
+    max_val = maximum(map(e->(weight(e) == Inf ? 0 : weight(e)), edges(graph)))
+    # get all shortest paths
+    for i=1:nv(graph)
+        paths_i = dijkstra_shortest_paths(graph, i)
+        push!(dist, paths_i.dists)
+        push!(paths, enumerate_paths(paths_i))
+    end
+    # find node with minimum shortest paths
+    min_avg, min_ind = findmin([sum(map(x->(x > max_val) ? max_val : x, d)) for d in dist])
+    min_paths = paths[min_ind]
+    subgraph = SimpleWeightedDiGraph(nv(graph))
+    weight_dict = to_dict(graph)
+    for path in min_paths
+        for i=2:length(path)
+            add_edge!(subgraph, path[i-1], path[i], weight_dict[path[i]][path[i-1]])
+        end
+    end
+    return (min_ind, subgraph, maximum([length(path) for path in min_paths]))
+end
+
+"""
+Loads an adjacency matrix from a file and stores it as a graph.
+You can specify a function to apply to each weight.
+This is often helpful in cases where the heuristic obeys the triangle inequality,
+to avoid the function from generating a star graph and mostly ignoring the heuristic.
+By default, the function is x->x^1.05, which allows the algorithm to
+split especially difficult registration problems into many steps.
+If this results in too many failed registrations, try increasnig the difficulty_importance parameter;
+conversely, if there is too much error accumulation over long chains of registrations, try decreasing it.
+# Arguments
+- `elastix_difficulty::String`: a text file containing a list of frames and an adjacency matrix.
+- `func` (optional): a function to apply to each element of the adjacency matrix
+- `difficulty_importance`: if `func` is not provided, it will be set to `x->x^(1+difficulty_importance)`
+Returns a graph `graph::SimpleWeightedGraph` storing the adjacency matrix.
+"""
+function load_graph(elx_difficulty::String; func=nothing, difficulty_importance::Real=0.05)
+    graph = nothing
+    imgs = nothing
+    if func == nothing
+        func = x->x^(1+difficulty_importance)
+    end
+    open(elx_difficulty) do f
+        count = 0
+        for line in eachline(f)
+            if count == 0
+                # the array of images
+                imgs = map(x->parse(Int64, x), split(replace(line, r"\[|\]|Any" => ""), ", "))
+                # number of nodes is the highest frame number
+                graph = SimpleWeightedGraph(maximum(imgs))
+            else
+                for e in enumerate(map(x->func(parse(Float64, x)), split(replace(line, r"\[|\]" => ""))))
+                    if e[2] != Inf && e[2] != 0
+                        # difficulty array may have skipped already-deleted frames
+                        # make sure to index into imgs to get accurate frame numbers
+                        add_edge!(graph, imgs[count], imgs[e[1]], e[2])
+                    end
+                end
+            end
+            count = count + 1
+        end
+    end
+    return graph
+end
+ 
+"""
+Outputs `graph::SimpleWeightedDiGraph` to an output file `outfile` containing a list of edges in `graph`.
+"""
+function output_graph(graph::SimpleWeightedDiGraph, outfile::String)
+    open(outfile, "w") do f
+        for edge in edges(graph)
+            write(f, string(Int16(src(edge)))*" "*string(Int16(dst(edge)))*"\n")
+        end
+    end
+end
+
+"""
+Deletes a problematic `frame::Integer` from a graph `graph::SimpleWeightedGraph` without changing frame indices.
+"""
+function remove_frame!(graph::SimpleWeightedGraph, frame::Integer)
+    # set weights of all edges from the frame to infinity
+    # thereby preventing it from being included in any shortest path
+    for n in neighbors(graph, frame)
+        add_edge!(graph, frame, n, Inf)
+    end
+end
+
