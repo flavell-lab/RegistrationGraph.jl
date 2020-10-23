325b6faaecce27ad807a22f580baaba4dc2f62ec
goldenratio1618 <adamatanas@gmail.com>
2020-10-23 15:29:36 -0400

    Added logging to array and fixed spacing issue

--- src/run_elastix.jl
+++ src/run_elastix.jl
@@ -121,13 +121,16 @@ function write_sbatch_graph(edges, data_dir_local::String, data_dir_remote::Stri
             elseif length(edges_in_arr) == array_size || i == length(edges)
                 # modify in case we finished all the scripts
                 modified_array_size = length(edges_in_arr)
-                array_str = "#!/bin/bash
+                array_str = ""
+                array_str *= "#!/bin/bash
                 #SBATCH --job_name=$(array_job_name)
+                #SBATCH --output=$(log_dir)/$(array_job_name)_$J.out
+                #SBATCH --error=$(log_dir)/$(array_job_name)_$J.err
                 #SBATCH --nodes=1
-                #SBATCH --array=1-$(modified_array_size)
                 #SBATCH --cpus-per-task=$(cpu_per_task)
                 #SBATCH --time=$(duration_str)
-                #SBATCH --mem=$(mem)G\n"
+                #SBATCH --mem=$(mem)G
+                #SBATCH --array=1-$(modified_array_size)\n"
                 if email != ""
                     array_str *= "#SBATCH --mail-user=$(email)
                     #SBATCH --mail-type=END\n"
