5a4b31924e2a635fa2d6d4b7f83f8cd0c5d628af
Adam Atanas <goldenratio1618@users.noreply.github.com>
2021-01-26 10:35:41 -0500

    updated channel definitions

--- src/registration_visualization.jl
+++ src/registration_visualization.jl
@@ -212,8 +212,9 @@ The fixed image will be red and the moving image will be green, so yellow indica
 - `swap_colors::Bool`: If set to `true`, fixed image will be green and moving image will be red.
 """
 function make_diff_pngs(param_path::Dict, param::Dict, get_basename::Function, fixed::Integer, moving::Integer;
-        proj_dim::Integer=3, fixed_ch::Integer=2, regdir_key::String="path_dir_reg", mhd_key="path_dir_mhd_filt", result::String="", 
+        proj_dim::Integer=3, fixed_ch_key::String="ch_marker", regdir_key::String="path_dir_reg", mhd_key="path_dir_mhd_filt", result::String="", 
         contrast_f::Real=1, contrast_m::Real=1, swap_colors::Bool=false)
+    fixed_ch = param[fixed_ch_key]
     reg_path = joinpath(param_path[regdir_key], string(moving) * "to" * string(fixed))
     fixed_img_path = joinpath(param_path[mhd_key], get_basename(fixed, fixed_ch)*".mhd")
     fixed_stack = dropdims(maximum(read_img(MHD(fixed_img_path)), dims=proj_dim), dims=proj_dim)
@@ -268,9 +269,11 @@ The fixed image will be red and the moving image will be green, so yellow indica
 - `swap_colors::Bool`: If set to `true`, fixed image will be green and moving image will be red.
 - `png_name::String`: Name of output file. Default `noreg.png`
 """
-function make_diff_pngs_base(param_path::Dict, get_basename::Function, fixed::Integer, moving::Integer;
-        proj_dim::Integer=3, regdir_key::String="path_dir_reg", mhd_key="path_dir_mhd_filt", moving_ch::Integer=2, fixed_ch::Integer=2,
-        contrast_f::Real=1, contrast_m::Real=1, swap_colors::Bool=false, png_name="noreg.png")
+function make_diff_pngs_base(param_path::Dict, param::Dict, get_basename::Function, fixed::Integer, moving::Integer;
+        proj_dim::Integer=3, regdir_key::String="path_dir_reg", mhd_key="path_dir_mhd_filt", moving_ch_key::String="ch_marker", 
+        fixed_ch_key::String="ch_marker", contrast_f::Real=1, contrast_m::Real=1, swap_colors::Bool=false, png_name="noreg.png")
+    fixed_ch = param[fixed_ch_key]
+    moving_ch = param[moving_ch_key]
     moving_img_path = joinpath(param_path[mhd_key], get_basename(moving, moving_ch)*".mhd")
     fixed_img_path = joinpath(param_path[mhd_key], get_basename(fixed, fixed_ch)*".mhd")
     png_path = joinpath(param_path[regdir_key], string(moving) * "to" * string(fixed), png_name)
